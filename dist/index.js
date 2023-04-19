a67d63f6babf45dda64ae70ba34b0d2151425609
Aleksandr Levochkin <v-allevockin@microsoft.com>
2023-04-19 15:44:23 +0200

    Extracted code frsgments to functions

--- dist/index.js
+++ dist/index.js
@@ -9654,42 +9654,46 @@ const core = __importStar(__nccwpck_require__(2186));
 const github = __importStar(__nccwpck_require__(5438));
 const getRepositoryData = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
+        const ownerName = core.getInput('owner-name');
         const repositoryName = core.getInput('repository-name');
-        console.log(repositoryName);
         const authToken = core.getInput('github-token');
         const octokit = github.getOctokit(authToken);
-        // Pulls
-        const pulls = yield octokit.rest.pulls.list({
-            owner: "microsoft",
-            repo: "typescript",
-            state: "all",
-        });
-        console.log(`Total PRs: ${pulls.data.length}`);
-        core.setOutput("total-pulls", pulls.data.length);
-        const openPulls = pulls.data.filter(pull => pull.state === "open");
-        console.log(`Open PRs: ${openPulls.length}`);
-        core.setOutput("open-pulls", openPulls.length);
-        const closedPulls = pulls.data.filter(pull => pull.state === "closed");
-        console.log(`Closed PRs: ${closedPulls.length}`);
-        core.setOutput("closed-pulls", closedPulls.length);
-        // Issues
-        const issues = yield octokit.rest.issues.listForRepo({
-            owner: "microsoft",
-            repo: "typescript",
-            state: "all",
-        });
-        console.log(`Total issues: ${issues.data}`);
-        core.setOutput("total-issues", issues.data.length);
-        const openIssues = issues.data.filter(issue => issue.state === "open");
-        console.log(`Open issues: ${openIssues.length}`);
-        core.setOutput("open-issues", openIssues.length);
-        const closedIssues = issues.data.filter(issue => issue.state === "closed");
-        console.log(`Closed issues: ${closedIssues.length}`);
-        core.setOutput("closed-issues", closedIssues.length);
+        yield setOutputForPulls(octokit, ownerName, repositoryName);
+        yield setOutputForIssues(octokit, ownerName, repositoryName);
     }
     catch (error) {
         core.setFailed(error instanceof Error ? error.message : "Exception occurred");
     }
+});
+const setOutputForPulls = (octokitClient, ownerName, repositoryName) => __awaiter(void 0, void 0, void 0, function* () {
+    const pulls = yield octokitClient.rest.pulls.list({
+        owner: ownerName,
+        repo: repositoryName,
+        state: "all",
+    });
+    console.log(`Total PRs: ${pulls.data.length}`);
+    core.setOutput("total-pulls", pulls.data.length);
+    const openPulls = pulls.data.filter(pull => pull.state === "open");
+    console.log(`Open PRs: ${openPulls.length}`);
+    core.setOutput("open-pulls", openPulls.length);
+    const closedPulls = pulls.data.filter(pull => pull.state === "closed");
+    console.log(`Closed PRs: ${closedPulls.length}`);
+    core.setOutput("closed-pulls", closedPulls.length);
+});
+const setOutputForIssues = (octokitClient, ownerName, repositoryName) => __awaiter(void 0, void 0, void 0, function* () {
+    const issues = yield octokitClient.rest.issues.listForRepo({
+        owner: ownerName,
+        repo: repositoryName,
+        state: "all",
+    });
+    console.log(`Total issues: ${issues.data.length}`);
+    core.setOutput("total-issues", issues.data.length);
+    const openIssues = issues.data.filter(issue => issue.state === "open");
+    console.log(`Open issues: ${openIssues.length}`);
+    core.setOutput("open-issues", openIssues.length);
+    const closedIssues = issues.data.filter(issue => issue.state === "closed");
+    console.log(`Closed issues: ${closedIssues.length}`);
+    core.setOutput("closed-issues", closedIssues.length);
 });
 getRepositoryData();
 
--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,57 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
+import { GitHub } from '@actions/github/lib/utils';
 
 const getRepositoryData = async () => {
     try {
+        const ownerName = core.getInput('owner-name');
         const repositoryName = core.getInput('repository-name');
-        console.log(repositoryName);
         
         const authToken = core.getInput('github-token');
         const octokit = github.getOctokit(authToken);
         
-        // Pulls
-        const pulls = await octokit.rest.pulls.list({
-            owner: "microsoft",
-            repo: "typescript",
-            state: "all",
-
-        });
-        console.log(`Total PRs: ${pulls.data.length}`);
-        core.setOutput("total-pulls", pulls.data.length);
-        
-        const openPulls = pulls.data.filter(pull => pull.state === "open");
-        console.log(`Open PRs: ${openPulls.length}`);
-        core.setOutput("open-pulls", openPulls.length);
-
-        const closedPulls = pulls.data.filter(pull => pull.state === "closed");
-        console.log(`Closed PRs: ${closedPulls.length}`);
-        core.setOutput("closed-pulls", closedPulls.length);
-
-        // Issues
-        const issues = await octokit.rest.issues.listForRepo({
-            owner: "microsoft",
-            repo: "typescript",
-            state: "all",
-        });
-        console.log(`Total issues: ${issues.data}`);
-        core.setOutput("total-issues", issues.data.length);
-
-        const openIssues = issues.data.filter(issue => issue.state === "open");
-        console.log(`Open issues: ${openIssues.length}`);
-        core.setOutput("open-issues", openIssues.length);
-
-        const closedIssues = issues.data.filter(issue => issue.state === "closed");
-        console.log(`Closed issues: ${closedIssues.length}`);
-        core.setOutput("closed-issues", closedIssues.length);
-
+        await setOutputForPulls(octokit, ownerName, repositoryName);
+        await setOutputForIssues(octokit, ownerName, repositoryName);
+       
       } catch (error) {
             core.setFailed(error instanceof Error ? error.message : "Exception occurred");
       }
+}
+
+const setOutputForPulls = async (octokitClient: InstanceType<typeof GitHub>, ownerName: string, repositoryName: string): Promise<void> => {
+    const pulls = await octokitClient.rest.pulls.list({
+        owner: ownerName,
+        repo: repositoryName,
+        state: "all",
+    });
+    console.log(`Total PRs: ${pulls.data.length}`);
+    core.setOutput("total-pulls", pulls.data.length);
+    
+    const openPulls = pulls.data.filter(pull => pull.state === "open");
+    console.log(`Open PRs: ${openPulls.length}`);
+    core.setOutput("open-pulls", openPulls.length);
+
+    const closedPulls = pulls.data.filter(pull => pull.state === "closed");
+    console.log(`Closed PRs: ${closedPulls.length}`);
+    core.setOutput("closed-pulls", closedPulls.length);
+}
+
+const setOutputForIssues = async (octokitClient: InstanceType<typeof GitHub>, ownerName: string, repositoryName: string): Promise<void> => {
+    const issues = await octokitClient.rest.issues.listForRepo({
+        owner: ownerName,
+        repo: repositoryName,
+        state: "all",
+    });
+    console.log(`Total issues: ${issues.data.length}`);
+    core.setOutput("total-issues", issues.data.length);
+
+    const openIssues = issues.data.filter(issue => issue.state === "open");
+    console.log(`Open issues: ${openIssues.length}`);
+    core.setOutput("open-issues", openIssues.length);
+
+    const closedIssues = issues.data.filter(issue => issue.state === "closed");
+    console.log(`Closed issues: ${closedIssues.length}`);
+    core.setOutput("closed-issues", closedIssues.length);
 }
 
 getRepositoryData();
