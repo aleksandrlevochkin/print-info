5baa8be23473e5ba46a1bdd44300dd70609e6801
Aleksandr Levochkin <v-allevockin@microsoft.com>
2023-04-19 16:26:15 +0200

    Test with smaller repo

--- src/index.ts
+++ src/index.ts
@@ -19,41 +19,46 @@ const getRepositoryData = async () => {
 }
 
 const setOutputForPulls = async (octokitClient: InstanceType<typeof GitHub>, ownerName: string, repositoryName: string): Promise<void> => {
-    octokitClient.paginate("GET /repos/{owner}/{repo}/pulls", {
+    const pulls = await octokitClient.paginate(octokitClient.rest.issues.listForRepo, {
         owner: ownerName,
         repo: repositoryName,
         state: "all",
-    }).then(pulls => {
-        console.log(`Total PRs: ${pulls.length}`);
-        core.setOutput("total-pulls", pulls.length);
-        
-        const openPulls = pulls.filter(pull => pull.state === "open");
-        console.log(`Open PRs: ${openPulls.length}`);
-        core.setOutput("open-pulls", openPulls.length);
-
-        const closedPulls = pulls.filter(pull => pull.state === "closed");
-        console.log(`Closed PRs: ${closedPulls.length}`);
-        core.setOutput("closed-pulls", closedPulls.length);
+        per_page: 100,
     });
+    
+    console.log(`Total PRs: ${pulls.length}`);
+    core.setOutput("total-pulls", pulls.length);
+        
+    const openPulls = pulls.filter(pull => pull.state === "open");
+    console.log(`Open PRs: ${openPulls.length}`);
+    core.setOutput("open-pulls", openPulls.length);
+
+    const closedPulls = pulls.filter(pull => pull.state === "closed");
+    console.log(`Closed PRs: ${closedPulls.length}`);
+    core.setOutput("closed-pulls", closedPulls.length);
 }
 
 const setOutputForIssues = async (octokitClient: InstanceType<typeof GitHub>, ownerName: string, repositoryName: string): Promise<void> => {
-    octokitClient.paginate(octokitClient.rest.issues.listForRepo, {
+    const issues = await octokitClient.paginate(octokitClient.rest.issues.listForRepo, {
         owner: ownerName,
         repo: repositoryName,
         state: "all",
-    }).then(issues => {
-        console.log(`Total issues: ${issues.length}`);
-        core.setOutput("total-issues", issues.length);
-
-        const openIssues = issues.filter(issue => issue.state === "open");
-        console.log(`Open issues: ${openIssues.length}`);
-        core.setOutput("open-issues", openIssues.length);
-
-        const closedIssues = issues.filter(issue => issue.state === "closed");
-        console.log(`Closed issues: ${closedIssues.length}`);
-        core.setOutput("closed-issues", closedIssues.length);
+        per_page: 100,
     });
+
+    const pulls = issues.filter(issue => issue.pull_request !== undefined);
+    console.log(`Total pull requests as filtered from issues: ${pulls.length}`);
+
+    console.log(`Total issues: ${issues.length}`);
+    core.setOutput("total-issues", issues.length);
+
+    const openIssues = issues.filter(issue => issue.state === "open");
+    console.log(`Open issues: ${openIssues.length}`);
+    core.setOutput("open-issues", openIssues.length);
+
+    const closedIssues = issues.filter(issue => issue.state === "closed");
+    console.log(`Closed issues: ${closedIssues.length}`);
+    core.setOutput("closed-issues", closedIssues.length);
 }
 
 getRepositoryData();
